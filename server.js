e603fd2d91054b0ad0e743b284bc4ed0f82c19ef
Chay Williams <Chayvw@gmail.com>
2020-07-06 23:59:29 -0400

    added notes route

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 // View Routes
 app.get("/", (req, res) => { 
- 
     res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+  app.get("notes", (req, res) => { 
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
 // 4. Listen on that port
