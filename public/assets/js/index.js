59e4feee72397cf4c9b145954408a8e26badbe15
Chay Williams <Chayvw@gmail.com>
2020-07-07 21:08:05 -0400

    got the app.post to work locally

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,6 +17,7 @@ const getNotes = () => {
 
 // A function for saving a note to the db
 const saveNote = (note) => {
+  console.log(note)
   return $.ajax({
     url: "/api/notes",
     data: note,
@@ -51,6 +52,7 @@ const renderActiveNote = () => {
 
 // Get the note data from the inputs, save it to the db and update the view
 const handleNoteSave = function () {
+  console.log("Handle Note");
   const newNote = {
     title: $noteTitle.val(),
     text: $noteText.val(),
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Require fs to read data from the files  
 const fs = require("fs");
 // an array for new notes 
-const newNotes =[];
+const newNotes = [];
 
 // 2. Create an instance of Express - app
 const app = express();
@@ -24,47 +24,56 @@ app.use(express.static("public"));
 
 
 // View Routes
-  app.get("/notes", (req, res) => { 
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-  app.get("*", (req, res) => { 
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-  // API Routes will GET and POST fs will read file res.json will 
-  
+
+// API Routes will GET and POST fs will read file res.json will 
+
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       return res.send("An error occurred reading your data.");
     }
-  
+    console.log(data);
+
     const newNotes = JSON.parse(data);
     res.json(newNotes);
   });
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 // New notes will be added 
 app.post("/api/notes", (req, res) => {
+  console.log(req.body);
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       return res.send("An error occurred reading your data.");
     }
     // manlipluate data
+    console.log(data);
     const newNotes = JSON.parse(data);
-    res.json(newNotes);
+    // res.json(newNotes);
     newNotes.push(req.body);
+    console.log(newNotes);
     // fs will write the data back
-    fs.writeFile("./db/db.json", JSON.stringifynewNotes, "utf8", (err, data) => {
+    fs.writeFile("./db/db.json", JSON.stringify(newNotes), "utf8", (err, data) => {
 
       if (err) {
         return res.send("An error occurred reading your data.");
       }
-      res.JSON(newNotes);
-  });
-});
+      res.json(newNotes);
 
+    });
+  });
+
+
+  
+});
 // 4. Listen on that PORT
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
-});
 });
